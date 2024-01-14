0c0a21ad9defd0ff44ef130490513bf58215cad2
Tony Knudson <tonyknudson91@gmail.com>
2024-01-14 02:19:10 -0600

    added new booking functionality

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,34 @@
 class BookingsController < ApplicationController
+    before_action :set_booking, only: %i[show]
+    def index
+        @bookings = Booking.all
+    end
+
+    def show
+    end
+
+    def new
+        @booking = Booking.new
+    end
+
+    def create
+        @booking = Booking.new(booking_params)
+        respond_to do |format|
+            if @booking.save
+                format.html { redirect_to booking_url(@booking), notice: "Booking created successfully" }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    private
+    
+    def set_booking
+        @booking = Booking.find(params[:id])
+    end
+
+    def booking_params
+        params.require(:booking).permit(:room_id, :start, :end, :purpose)
+    end
 end
