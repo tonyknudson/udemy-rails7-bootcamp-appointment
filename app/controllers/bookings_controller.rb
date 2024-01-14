64b2653c5fba6d92c99cb2eb2459631ebc550339
Tony Knudson <tonyknudson91@gmail.com>
2024-01-14 02:23:44 -0600

    added booking edit functionality

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-    before_action :set_booking, only: %i[show]
+    before_action :set_booking, only: %i[show edit update]
     def index
         @bookings = Booking.all
     end
@@ -22,6 +22,19 @@ class BookingsController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        respond_to do |format|
+            if @booking.update(booking_params)
+                format.html { redirect_to booking_url(@booking), notice: "Booking updated successfully" }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+            end
+        end
+    end
+    
     private
     
     def set_booking
