e82ed79480c2cc5dc3256e7ee61c355e4831e3f7
Tony Knudson <tonyknudson91@gmail.com>
2024-01-14 03:24:04 -0600

    added authenication requirement to bookings view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
     before_action :set_booking, only: %i[show edit update destroy]
+    before_action :authenticate_user!
     def index
         @bookings = Booking.all
     end
